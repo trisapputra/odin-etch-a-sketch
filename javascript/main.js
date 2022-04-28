7b7cb5b0237abd2904fd10a0a6b850d1fe534258
tri <trin.saputra@gmail.com>
2022-04-28 21:17:05 +0700

    add for loops eventlistener default button for append first child canvas

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,11 @@
 const defaultButton = document.getElementById("default-button");
-defaultButton.addEventListener('click', (e) => {
-    console.log(e)
+defaultButton.addEventListener('click', () => {
+    
+    const canvasContainer = document.getElementById("canvas-container");
+
+    for (var i = 0; i < 16; i++){
+        const div = document.createElement("div");
+        div.classList.add("canvasGrid");
+        canvasContainer.appendChild(div);
+    }
 });
